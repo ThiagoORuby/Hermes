d7e486cabf2c3ce9ce6f308acb5773b6d7e4aef4
ThiagoORuby <trs@ic.ufal.br>
2024-06-22 17:45:09 -0300

    feat(backend): create Scrapy base files

---
+++ backend/app/news_scrapper/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+import scrapy.statscollectors
+
+
+class PostItem(scrapy.Item):
+    image_url = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    description = scrapy.Field()
+    datetime = scrapy.Field()
+    type = scrapy.Field()
---
+++ backend/app/news_scrapper/pipelines.py
@@ -0,0 +1,52 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+# useful for handling different item types with a single interface
+from itemadapter.adapter import ItemAdapter
+
+from models import Post
+from services.db import Session
+
+
+class NewsScrapperPipeline:
+
+    def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name == "type":
+                value = adapter.get(field_name)
+                if value:
+                    adapter[field_name] = value.lower()
+            if field_name == "description":
+                value = adapter.get(field_name)
+                if not value:
+                    adapter[field_name] = ""
+
+        return item
+
+
+class PostgresPipeline:
+
+    def __init__(self):
+        self.session = Session()
+
+    def process_item(self, item, spider):
+
+        if self.session.query(Post).filter_by(url=item["url"]).first():
+            print("Has already been saved")
+            return item
+
+        post = Post(**item)
+
+        self.session.add(post)
+        self.session.commit()
+
+        return item
+
+    def close_spider(self, spider):
+        self.session.close()
